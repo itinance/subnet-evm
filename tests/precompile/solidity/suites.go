017ec2d9d08505950df9e9e184f65c442f9f8497
Hagen Hübel <hhuebel@itinance.com>
2023-01-29 15:08:37 +0100

    Step 8.3: Declaring the HardHat E2E Test

--- tests/precompile/solidity/suites.go
+++ tests/precompile/solidity/suites.go
@@ -61,6 +61,15 @@ var _ = ginkgo.Describe("[Precompiles]", ginkgo.Ordered, func() {
 		utils.ExecuteHardHatTestOnNewBlockchain(ctx, "reward_manager")
 	})
 
+	ginkgo.It("hello world", ginkgo.Label("Precompile"), ginkgo.Label("HelloWorld"), func() {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+		defer cancel()
+
+		// Specify the name shared by the genesis file in ./tests/precompile/genesis/{your_precompile}.json
+		// and the test file in ./contract-examples/tests/{your_precompile}.ts
+		utils.ExecuteHardHatTestOnNewBlockchain(ctx, "hello_world")
+	})
+
 	// TODO: can we refactor this so that it automagically checks to ensure each hardhat test file matches the name of a hardhat genesis file
 	// and then runs the hardhat tests for each one without forcing precompile developers to modify this file.
 	// ADD YOUR PRECOMPILE HERE
